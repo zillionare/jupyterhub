5ea2c57c7f09c06a057502e5f0bf767997922737
aaron yang <aaron_yang@jieyu.ai>
2023-03-13 21:27:25 +0800

    fixed notebook无法保存问题

--- deployment/adduser.sh
+++ deployment/adduser.sh
@@ -8,6 +8,9 @@ else
     docker run -d -v /data/course/notebooks/courseware:/home/$user/notebooks/courseware:ro -v /data/course/notebooks/examples:/home/$user/notebooks/examples:ro --name course_$user -e user=$user -e password=$user@cH --net=course zillionare/courselab
 fi
 
+# 增加欢迎文档
+docker cp ./欢迎使用大富翁量化环境.ipynb "course_$user:/home/$user/notebooks"
+
 # add nginx conf
 sed "s/aaron/$user/g" user.conf.template > /opt/nginx/conf/users/"$user.conf"
 docker restart nginx
