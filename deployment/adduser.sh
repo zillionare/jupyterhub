2775a7bdcbe39c2c0078bf79923f6f59f2e63662
aaron yang <aaron_yang@jieyu.ai>
2023-03-26 16:38:15 +0800

    fix: update adduser according to new image

--- deployment/adduser.sh
+++ deployment/adduser.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 export user=$1
-if [ "$2" = "rw" ]
-then 
-    docker run -d -v /data/course/notebooks/courseware:/home/$user/notebooks/courseware -v /data/course/notebooks/examples:/home/$user/notebooks/examples --name course_$user -e user=$user -e password=$user@cH --net=course zillionare/courselab
+if [ "$2" = "payed" ]
+then
+    docker run -d -v /data/course/notebooks/courseware:/home/$user/notebooks/courseware:ro -v /data/course/notebooks/examples:/home/$user/notebooks/examples:ro --name course_$user -e user=$user -e password=$user@cH --net=course zillionare/courselab:0.2
 else
-    docker run -d -v /data/course/notebooks/courseware:/home/$user/notebooks/courseware:ro -v /data/course/notebooks/examples:/home/$user/notebooks/examples:ro --name course_$user -e user=$user -e password=$user@cH --net=course zillionare/courselab
+    docker run -d -v /data/course/notebooks/examples:/home/$user/notebooks/examples:ro --name course_$user -e user=$user -e password=$user@cH --net=course zillionare/courselab:0.2
 fi
 
 # 增加欢迎文档
-docker cp ./欢迎使用大富翁量化环境.ipynb "course_$user:/home/$user/notebooks"
+docker cp ./使用说明.ipynb "course_$user:/home/$user/notebooks"
 
 # add nginx conf
 sed "s/aaron/$user/g" user.conf.template > /opt/nginx/conf/users/"$user.conf"
