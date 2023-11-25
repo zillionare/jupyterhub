a716829bcf1bec854e42dbbb536692e8b74f5568
aaron yang <aaron_yang@jieyu.ai>
2023-11-25 16:25:37 +0800

    🐞 fix: jupyter升级后，没有notebook.auth模块

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-jupyter-lab --NotebookApp.password=`python -c "from notebook.auth import passwd; print(passwd('$LAB_PASSWORD'))"` --ip='*' --allow-root --no-browser --notebook-dir="/root/notebooks" --NotebookApp.base_url="/$LAB_USER/"
+jupyter-lab --NotebookApp.password=`python -c "from jupyter_server.auth import passwd; print(passwd('$LAB_PASSWORD'))"` --ip='*' --allow-root --no-browser --notebook-dir="/root/notebooks" --NotebookApp.base_url="/$LAB_USER/"
